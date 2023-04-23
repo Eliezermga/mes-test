4b00cee59dfc7a6b45e2abbb39a7a1b1b32d9f83
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-04-23 22:59:40 +0200

    script débutant

---
+++ script.js
@@ -0,0 +1,21 @@
+
+    var m=prompt("quelle valeur voulez vous prendre pour faire un test avec l'operateur conditionnel ternaire");
+    (m<2)? alert("la valeur est inferieur 2"): alert("la valeur est superieur à 2")
+        nom = prompt("veuillez saisir votre nom!","");
+        alert("hereux de faire votre connaissance, "+nom+" !");
+        x=5
+        alert(++x)
+        /*incrementation de 1 à la valeur */
+
+        var ok = 'bonjour',name=' à toi';
+        resultat = ok + name
+        alert(resultat);
+        /*la concatenation se fait avec le signe + */
+
+        suit = confirm("voulez vous poursuivre ?");
+            if (suit==true){
+                alert(" j'en suis hereux");
+            }
+            else{
+                alert("c'est dommage")
+            }
