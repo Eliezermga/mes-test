0a3677a03cc1c38c758a3a4077536958cb2a45c6
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-05-02 16:16:41 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,3 +1,6 @@
+var T = prompt("veuillez ecrire la temperature");
+(T>37)? alert("accès au cheking approfondi"): alert("bonne santé");
+
 function reste(nombre);
     nombre = 8754
     cons1 = nombre/1000
