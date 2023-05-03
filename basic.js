407682e83fc2787e87039a0678f0fe5f9c98986e
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-05-03 11:20:14 +0200

    basic

---
+++ basic.js
@@ -0,0 +1,31 @@
+/*const pi = 3.14;
+    pi = 1;
+    console.log(pi)
+
+var a = 3;
+    a = 3;
+    console.log(a)
+console.log(30*24*60*60)
+
+var L = 14;
+var l = 8;
+aire = L*l;
+console.log(aire)
+
+var b = "unh";
+    console.log(`ici c'est ${b}`);*/
+
+
+ entrez = prompt("entrer le solde de votre solde");
+ retrait = prompt("montant à retirer");
+ solde = entrez - retrait;
+ alert ("vous avez retirez "+retrait  +" votre solde est de :" +solde);
+
+ switch (key) {
+    case value:
+        
+        break;
+ 
+    default:
+        break;
+ }
