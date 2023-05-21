217faabf2c29a02c1e19a5f2117f4b6c9574d159
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-05-21 16:32:51 +0200

    script

---
+++ js/operateur.js
@@ -0,0 +1,24 @@
+let x = 8;
+let y = 3;
+let f = 9;
+let e = 1/2;
+console.log(x+y);//addition
+console.log(x-y);//subtraction
+console.log(x*y);//multiplication
+console.log(x/y);//division
+console.log(x**y);//exposant
+console.log(f**e);//racine carré
+
+let balls = 100;
+let boxes = 3;
+console.log(balls % boxes);//reste de la divission
+
+let count = 0;
+count++;
+console.log(count);//incrementation
+
+let score = 100;
+score--;
+score--;
+console.log(score);//decrementation
+
