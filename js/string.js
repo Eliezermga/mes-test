3d2ab8bb327ac265574541b6ed072d644de29fa4
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-05-22 23:42:26 +0200

    basic

--- js/string.js
+++ js/string.js
@@ -3,3 +3,7 @@ let msg2 = 'Keep it up';
 
 console.log("I'm happy!");
 console.log('She said "Yes!"');
+
+console.log("I'm happy"); //error
+console.log('I\'m happy');
+console.log("She said \"Yes!\"");
