12611cbd7e9cf46939a5398ce6cbe4da45af88a6
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-06-16 19:17:18 +0200

    py

---
+++ PYTHON/DECTMAJ.py
@@ -0,0 +1,20 @@
+a=input('entrer un mot:')
+def detect(a):
+    nouv=""
+    maj=''
+    taille=len(a)
+    for i in range (taille):
+        if ord(a[i])>=65 and ord(a[i]) <= 90 :
+            maj+=(a[i])
+            maj+=" "
+            x=ord(a[i])
+            y=x+32
+            n=chr(y)
+            nouv+=n
+        else:
+            nouv+=a[i]
+    print(maj)
+    print(nouv)
+    return nouv
+
+detect(a)
