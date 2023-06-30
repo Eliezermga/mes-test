6049ef00597e5b27f07ffe6a697ce8a2afa998d0
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-06-30 15:18:49 +0200

    back-ticks

--- js/litereau.js
+++ js/litereau.js
@@ -1,2 +1,14 @@
 let temp = `Hello, user!`;
 console.log(temp)
+
+console.log(`I'm happy, she said "Yes"!`);
+let msg = `Hey! Are you sololearning?
+Isn't it awesome?`;
+
+console.log(msg)
+
+let name = "John";
+let text = `Welcome, ${name}`;
+console.log(text);
+
+/* le modèle literreau utilise le symbole appelé back-ticks */
