de01874d3271e70d36500e721637abf2f8f6ed3b
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-07-09 10:17:19 +0200

    if

--- js/if.js
+++ js/if.js
@@ -5,3 +5,16 @@ let minPoints = 100;
 if(points>=minPoints){
     console.log(`Well done, ${player}!`);
 }
+
+let color ="yellow";
+
+switch(color) {
+  case "blue": 
+    console.log("This is blue.");
+    break;
+  case "red": 
+    console.log("This is red.");
+    break;
+  default: 
+    console.log("Color not found.");
+}
