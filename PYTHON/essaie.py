ad8cb5e78997a9a42c6cbe987cc9ecf2b904eae8
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-08-31 12:10:55 +0200

    ..

---
+++ PYTHON/essaie.py
@@ -0,0 +1,16 @@
+"""a, b, r, q=4, -1,2,6
+a = 19
+b = 6
+r=+a-r
+a=+3
+print(r)
+q+=1
+r+=a-r
+"""
+mot_de_passe = "primature234"
+cle = input('entrez le mot de passe : ')
+if cle == mot_de_passe :
+    print("bienvenue")
+else :
+    
+
